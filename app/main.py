325a72eb4dfff1699a7b928f2bf5bee4c4f27d6c
Идрисов Дамир <idrisovdamir88@gmail.com>
2025-05-04 21:45:19 +0300

    Почистил

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Инициализация базы данных
+
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Telegram Todo Bot API")
